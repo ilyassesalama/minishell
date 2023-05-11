b3253f903069185b889ec2a1844ce123b866355b
Ilyasse <ilyassesalama7@gmail.com>
2023-05-11 19:27:49 +0100

    fix: Removed useless codes

--- minishell.c
+++ minishell.c
@@ -3,21 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tajjid <tajjid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: isalama <isalama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 16:10:08 by isalama           #+#    #+#             */
-/*   Updated: 2023/05/11 19:25:42 by tajjid           ###   ########.fr       */
+/*   Updated: 2023/05/11 19:27:37 by isalama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-typedef struct s_cum
-{
-	int		a;
-	int		b;
-	char	*str;
-}				cum;
 
 int main()
 {
@@ -27,4 +20,3 @@ int main()
 		read(0, 0, 1);
 	}
 }
-albgbz
