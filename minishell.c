77982c6fa9e0464edbfd785aa2c82af782b357fa
Taha Ajjid <cyphertaha@gmail.com>
2023-06-18 00:10:27 +0100

    feat: Added command filler

--- minishell.c
+++ minishell.c
@@ -3,23 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isalama <isalama@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tajjid <tajjid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 16:10:08 by isalama           #+#    #+#             */
-/*   Updated: 2023/05/20 03:22:22 by isalama          ###   ########.fr       */
+/*   Updated: 2023/06/17 23:59:02 by tajjid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // CHOOSE THE PROJECT TYPE
-# define ARE_WE_EXECUTING true
+# define ARE_WE_EXECUTING false
 
 void are_we_executing(t_env *env, t_token *tokens)
 {
-	if(ARE_WE_EXECUTING){
+	if(ARE_WE_EXECUTING)
 		tokens_execution(env);
-	} else {
+	else 
+	{
 		t_token *temp = tokens;
 		while (temp != NULL)
 		{
@@ -31,6 +32,8 @@ void are_we_executing(t_env *env, t_token *tokens)
 
 void scan_input(char *input, t_env *env)
 {
+	int i = 0;
+	t_command *commands;
 	if(!handle_quotes(input)){
 		return;
 	}
@@ -39,9 +42,21 @@ void scan_input(char *input, t_env *env)
 	tokens = tokens_creation(input, env);
 	if (tokens == NULL)
 		return ;
-		
+	commands = command_creator(tokens);
+	while (commands)
+	{
+		i = 0;
+		printf("--------------------\n");
+		printf("{ command: %s input: %d output: %d }\n", commands->command, commands->input, commands->output);
+		while (commands->args[i])
+		{
+			printf("arg: %s\n", commands->args[i]);
+			i++;
+		}
+		printf("--------------------\n");
+		commands = commands->next;
+	}
 	are_we_executing(env, tokens);
-	
 	ft_t_lstclear(&tokens);
 }
 
@@ -52,7 +67,7 @@ int main(int argc, char **argv, char **env)
 	t_env *env_list;
 	int i;
 	char *receiver;
-	char *input_command = "\033[1;31m➜ \033[1;32m minishell: \033[0m";
+	char *input_command = " minishell: ";
 
 	i = 0;
 	while (env[i] != NULL && ft_strncmp(env[i], "PATH=", 5) != 0)
@@ -65,7 +80,6 @@ int main(int argc, char **argv, char **env)
 		ft_lstadd_back(&env_list, ft_envlstnew(ft_strdup(temp[0]), ft_strdup(temp[1])));
 		i++;
 	}
-		
 	while (true)
 	{
 		receiver = readline(input_command);
