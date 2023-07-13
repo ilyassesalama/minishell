be9579b0c0590572a9c0af5103e90b9a8ebc60b3
Ilyasse <ilyassesalama7@gmail.com>
2023-07-13 19:26:36 +0100

    norm: Fixed minishell.c norm

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: isalama <isalama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 12:04:45 by tajjid            #+#    #+#             */
-/*   Updated: 2023/07/13 17:54:07 by isalama          ###   ########.fr       */
+/*   Updated: 2023/07/13 19:26:01 by isalama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,27 +49,35 @@ void	scan_input(char *input, t_env **env)
 	ft_c_lstclear(&commands);
 }
 
-int	main(int argc, char **argv, char **env)
+void	fill_envirement(t_env **env_list, char **env)
 {
-	t_env	*env_list;
 	int		i;
-	char	*receiver;
-	char	*input_command;
+	char	**temp;
 
-	signals_handler();
-	input_command = "\x1b[32m minishell: \x1b[0m";
-	(void)argc;
-	(void)argv;
 	i = 0;
 	while (env[i] != NULL && ft_strncmp(env[i], "PATH=", 5) != 0)
 		i++;
 	i = 0;
 	while (env[i] != NULL)
 	{
-		char **temp = ft_split(env[i], '=');
-		ft_lstadd_back(&env_list, ft_envlstnew(ft_strdup(temp[0]), ft_strdup(temp[1])));
+		temp = ft_split(env[i], '=');
+		ft_lstadd_back(env_list, ft_envlstnew(ft_strdup(temp[0]), ft_strdup(temp[1])));
 		i++;
 	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	t_env	*env_list;
+	char	*receiver;
+	char	*input_command;
+
+	(void)argc;
+	(void)argv;
+	env_list = NULL;
+	input_command = "\x1b[32m minishell: \x1b[0m";
+	signals_handler();
+	fill_envirement(&env_list, env);
 	while (true)
 	{
 		receiver = readline(input_command);
