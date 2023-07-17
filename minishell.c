f155f8af89c32156a3f92331e6be19bc1ce12012
Ilyasse <ilyassesalama7@gmail.com>
2023-07-17 02:31:48 +0100

    feat: Normed minishell.c

--- minishell.c
+++ minishell.c
@@ -3,47 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tajjid <tajjid@student.42.fr>              +#+  +:+       +#+        */
+/*   By: isalama <isalama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 12:04:45 by tajjid            #+#    #+#             */
-/*   Updated: 2023/07/17 01:58:37 by tajjid           ###   ########.fr       */
+/*   Updated: 2023/07/17 02:30:53 by isalama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-# define ARE_WE_EXECUTING false
-
-void	print_tokens(t_token *tokens)
-{
-	while (tokens)
-	{
-		printf("token: %s  type: %d\n", tokens -> content, tokens -> type);
-		tokens = tokens -> next;
-	}
-}
-
-// void	print_commands(t_command *commands)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (commands)
-// 	{
-// 		printf("------------------------------------\n");
-// 		printf("command: %s\n", commands -> command);
-// 		printf("input: %d\n", commands -> input);
-// 		printf("output: %d\n", commands -> output);
-// 		while (commands -> args[i])
-// 		{
-// 			printf("arg[%d] : %s\n", i, commands -> args[i]);
-// 			i++;
-// 		}
-// 		i = 0;
-// 		commands = commands -> next;
-// 		printf("------------------------------------\n");
-// 	}
-// }
 
 void	scan_input(char *input, t_env **env)
 {
@@ -57,36 +24,49 @@ void	scan_input(char *input, t_env **env)
 	tokens = tokens_creation(input, *env);
 	if (tokens == NULL)
 		return ;
-	// print_tokens(tokens);
 	commands = command_creator(tokens, *env);
-	if (ARE_WE_EXECUTING)
-		tokens_execution(commands, env);
-	else
-		print_commands(commands);
+	tokens_execution(commands, env);
 	ft_t_lstclear(&tokens);
 	ft_c_lstclear(&commands);
 }
 
-int	main(int argc, char **argv, char **env)
+void	init_readline(t_env **env_list)
 {
-	t_env	*env_list;
 	int		i;
 	char	*receiver;
-	char	*input_command;
-	char	**temp;
 
+	i = 0;
+	while (true)
+	{
+		signals_handler();
+		g_global.heredoc_eof = 0;
+		receiver = readline("\x1b[32m minishell: \x1b[0m");
+		if (receiver == NULL)
+			break ;
+		if (ft_strlen(receiver) > 0)
+			add_history(receiver);
+		scan_input(receiver, env_list);
+		signals_handler();
+	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	int		i;
+	char	**temp;
+	t_env	*env_list;
+
+	env_list = NULL;
+	i = 0;
 	if (argc != 1)
-		return (0);
+		return (1);
 	signals_handler();
 	rl_catch_signals = 0;
 	g_global.heredoc_eof = 0;
-	input_command = "\x1b[32m minishell: \x1b[0m";
 	(void)argc;
 	(void)argv;
-	i = 0;
 	while (env[i] != NULL && ft_strncmp(env[i], "PATH=", 5) != 0)
 		i++;
-	i = 0;
 	while (env[i] != NULL)
 	{
 		temp = ft_split(env[i], '=');
@@ -94,16 +74,5 @@ int	main(int argc, char **argv, char **env)
 			ft_envlstnew(ft_strdup(temp[0]), ft_strdup(temp[1])));
 		i++;
 	}
-	while (true)
-	{
-		signals_handler();
-		g_global.heredoc_eof = 0;
-		receiver = readline(input_command);
-		if (receiver == NULL)
-			break ;
-		if (ft_strlen(receiver) > 0)
-			add_history(receiver);
-		scan_input(receiver, &env_list);
-		signals_handler();
-	}
+	init_readline(&env_list);
 }
