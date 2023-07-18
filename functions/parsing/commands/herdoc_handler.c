0c2e42e183f3522fba4cdb162e797c0614849a9c
Taha Ajjid <cyphertaha@gmail.com>
2023-07-18 23:30:46 +0100

    fix: Fixed double dollar infinite loop

--- functions/parsing/commands/herdoc_handler.c
+++ functions/parsing/commands/herdoc_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   herdoc_handler.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isalama <isalama@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tajjid <tajjid@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 22:51:22 by tajjid            #+#    #+#             */
-/*   Updated: 2023/07/18 06:27:01 by isalama          ###   ########.fr       */
+/*   Updated: 2023/07/18 22:43:37 by tajjid           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 void	heredoc_readline(int output, char *limiter, t_env *env, int expand)
 {
 	char	*line;
+	char	*tmp;
 
+	(void)env;
+	(void)expand;
+	tmp = NULL;
 	line = NULL;
 	while (1 && g_global.heredoc_eof == 0)
 	{
@@ -24,8 +28,9 @@ void	heredoc_readline(int output, char *limiter, t_env *env, int expand)
 			break ;
 		if (expand)
 		{
-			while (line && ft_strchr(line, '$'))
-				line = d_quote_expander(line, env);
+			tmp = line;
+			line = d_quote_expander(tmp, env);
+			free(tmp);
 		}
 		write(output, line, ft_strlen(line));
 		write(output, "\n", 1);
@@ -65,19 +70,41 @@ bool	heredoc_expand_checker(t_token **tmp_tokens,
 	return (expand);
 }
 
+char	*tmp_file_name(void)
+{
+	char	*tmp_file;
+	char	*fixed_file;
+	int		i;
+
+	i = 1;
+	fixed_file = ft_strdup("/tmp/tmp_file");
+	tmp_file = ft_strdup(fixed_file);
+	while (access(tmp_file, F_OK) == 0)
+	{
+		free(tmp_file);
+		tmp_file = ft_strjoin(fixed_file, ft_itoa(i), 2);
+		i++;
+	}
+	free(fixed_file);
+	return (tmp_file);
+}
+
 int	heredoc_handler(t_token **tmp_tokens, int input,
 			int output, t_env *env)
 {
 	bool	expand;
 	char	*limiter;
+	char	*tmp_file;
 
 	herdoc_signal();
 	expand = false;
 	limiter = NULL;
 	expand = heredoc_expand_checker(tmp_tokens, expand, &limiter);
-	output = fd_opener("/tmp/tmp.txt", 0);
+	tmp_file = tmp_file_name();
+	output = fd_opener(tmp_file, 0);
 	heredoc_readline(output, limiter, env, expand);
-	input = fd_opener("/tmp/tmp.txt", 2);
+	input = fd_opener(tmp_file, 2);
+	free(tmp_file);
 	free(limiter);
 	return (input);
 }
